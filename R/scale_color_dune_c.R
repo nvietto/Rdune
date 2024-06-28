34a77846390048f9ff8734d77fca184467542033
nvietto <nvietto@user.noreply.github.com>
2024-06-28 17:06:42 -0500

    added a stop to improve testing

--- R/scale_color_dune_c.R
+++ R/scale_color_dune_c.R
@@ -5,6 +5,12 @@ scale_color_dune_c <- function(name,  ...) {
     stop("Palette name not found. Please provide a valid palette name or check the spelling.")
   }
 
+  palette_choice <- dune_palettes[[name]]
+
+  if (is.null(palette_choice) || is.numeric(name)) {
+    stop("Palette does not exist.")
+  }
+
   ggplot2::scale_color_gradientn(
     colors = dune_palette(name = name),
     ...
--- R/scale_fill_dune_d.R
+++ R/scale_fill_dune_d.R
@@ -5,6 +5,12 @@ scale_fill_dune_d <- function(name,  ...) {
     stop("Palette name not found. Please provide a valid palette name or check the spelling.")
   }
 
+  palette_choice <- dune_palettes[[name]]
+
+  if (is.null(palette_choice) || is.numeric(name)) {
+    stop("Palette does not exist.")
+  }
+
   ggplot2::discrete_scale(aesthetics = "fill",
                           palette = function(n) dune_palette(name = name, n = n),
                           ...)
