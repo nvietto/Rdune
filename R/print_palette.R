fb6a4288e44e48b3dd1012a9f52098cde8e86964
nvietto <nvietto@user.noreply.github.com>
2024-07-01 17:33:21 -0500

    small edits

--- R/print_palette.R
+++ R/print_palette.R
@@ -7,7 +7,7 @@
 
 print.palette <- function(x, ...) {
   n <- length(x)
-  old <- graphics::par(mar = c(0, 0, 0, 0))
+  old <- graphics::par(mar = c(0., 0.2, 0.2, 0.2))
   on.exit(graphics::par(old))
 
   graphics::image(1:n, 1, as.matrix(1:n),
@@ -15,7 +15,7 @@ print.palette <- function(x, ...) {
                   ylab = "", xaxt = "n", yaxt = "n", bty = "n")
 
   graphics::rect(0, 0.9, n + 1, 1.1,
-                 col = grDevices::rgb(0, 0, 0, 0.8),
+                 col = grDevices::rgb(0.8, 0.8, 0.8, 0.5),
                  border = NA)
 
   graphics::text((n + 1) / 2, 1,
